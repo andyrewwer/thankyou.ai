36b45bde47e9489f9e8a9e62fc11ef07954e10e7
Andrew Weeks <ajw@enfuse.io>
2023-01-21 13:52:09 -0700

    Small fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 module.exports = {
     assetPrefix: isGithubActions ? '/thankyou.ai/' : '',
     images: {
-        unoptimized: isGithubActions,
+        unoptimized: !!isGithubActions,
     },
 }
