573bfc81b4a774657793680383cdd7837880aebe
Andrew Weeks <ajw@enfuse.io>
2023-01-21 15:17:51 -0700

    Do we need that?

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-module.exports = {
-    assetPrefix: isGithubActions ? '/thankyou.ai/' : '',
-    images: {
-        unoptimized: !!isGithubActions,
-    },
-}
